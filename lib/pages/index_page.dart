d3312b9445a3c2ede43dc4974b206f9d5b67074b
许兆通 <xuzhaotong@msfintech.com>
2020-03-09 19:03:51 +0800

    1

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
 
 class IndexPage extends StatefulWidget {
   @override
@@ -25,8 +29,37 @@ class _IndexPageState extends State<IndexPage> {
       title: Text("会员中心"),
     ),
   ];
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+  int _currentIndex = 0;
+  var _currentPage;
+
+  @override
+  void initState() {
+    _currentPage = tabBodies[_currentIndex];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _currentPage = tabBodies[index];
+          });
+        },
+      ),
+      body: _currentPage,
+    );
   }
 }

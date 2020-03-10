e02c808dbdfe6eb2640f526711720993b52e540e
XuZhaoTong <358007437@qq.com>
2020-03-10 23:11:53 +0800

    1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
@@ -18,12 +19,15 @@ class _HomePageState extends State<HomePage> {
           future: getHomePageContent(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              var _data = json.decode(snapshot.data.toString());
+              var data = json.decode(snapshot.data.toString());
               List<Map> _swiperDataList =
-                  (_data['data']['slides'] as List).cast(); // 顶部轮播组件数
+                  (data['data']['slides'] as List).cast(); //顶部轮播组件数
+              List<Map> _navgatorDataList =
+                  (data['data']['category'] as List).cast();
               return Column(
                 children: <Widget>[
-                  SwiperDiy(swiperDataList: _swiperDataList), //页面顶部轮播组件
+                  SwiperDiy(swiperDataList: _swiperDataList),
+                  TopNavigator(navgatorDataList: _navgatorDataList), //页面顶部轮播组件
                 ],
               );
             } else {
@@ -39,19 +43,64 @@ class _HomePageState extends State<HomePage> {
 // 首页轮播组件编写
 class SwiperDiy extends StatelessWidget {
   final List swiperDataList;
-
   SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context, int index) {
-          return Image.network("${swiperDataList[index]['image']}");
+          return Image.network("${swiperDataList[index]['image']}",
+              fit: BoxFit.fill);
         },
         itemCount: swiperDataList.length,
         pagination: SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navgatorDataList;
+  TopNavigator({this.navgatorDataList});
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+        print('点击了导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item['image'],
+            width: ScreenUtil().setWidth(95),
+          ),
+          Text(
+            item['mallCategoryName'],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (navgatorDataList.length > 10) {
+      navgatorDataList.removeRange(10, navgatorDataList.length);
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5),
+        children: navgatorDataList.map(
+          (item) {
+            return _gridViewItemUI(context, item);
+          },
+        ).toList(),
       ),
     );
   }
